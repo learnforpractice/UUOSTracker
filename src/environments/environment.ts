e9ea48e7999cc1e61a598568553d757022fa2014
GooeyRiver <monster-susan@qq.com>
2019-01-22 16:29:48 +0800

    Update theme color

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   walletUrl: 'https://walleteos.com',
   votingUrl: 'https://eosportal.io',
   appName: 'UUOS Network',
-  logoUrl: '/assets/logo.jpeg',
+  logoUrl: '/assets/logo.png',
   blockchainUrl: 'http://116.206.178.22:8888',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 };
