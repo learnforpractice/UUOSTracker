ad73935c92b21741dd8bdf17b53dab62cb057800
GooeyRiver <monster-susan@qq.com>
2019-04-15 11:09:51 +0800

    Update link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
   appName: 'UUOS Network',
   logoUrl: '/assets/logo.png',
   logoUrl1: '/assets/logo_blue.png',
-  blockchainUrl: 'http://116.206.178.22:8888',
+  blockchainUrl: 'http://nodes.uuos.network:8880',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 };
