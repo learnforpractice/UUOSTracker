f6b2fe33cdd4daf119e4659687e8e21180ce8474
learnforpractice <learnforpractice@gmail.com>
2019-10-24 11:48:18 +0800

    change blockchainUrl to https://testapi.uuos.network

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'UUOS Network',
   logoUrl: '/assets/logo_blue.png',
-  blockchainUrl: 'https://nodes.uuos.network',
+  blockchainUrl: 'https://testapi.uuos.network',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 };
