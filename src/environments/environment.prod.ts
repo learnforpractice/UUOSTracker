9e6597d8d8ddd1ad79c8658d221fa1134f3b9c71
learnforpractice <learnforpractice@gmail.com>
2019-10-24 12:26:38 +0800

    UUOS Network - UUOS TestNet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   walletUrl: 'https://walleteos.com',
   votingUrl: 'https://eosportal.io',
-  appName: 'UUOS Network',
+  appName: 'UUOS TestNet',
   logoUrl: '/assets/logo_blue.png',
   blockchainUrl: 'https://testapi.uuos.network',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
